e623b22c7cc104a9b82c482fdea6543d01ec789d
Zirzeus <79276659+Maximiliano-Zerecero@users.noreply.github.com>
2024-11-08 08:35:05 -0700

    repaired buttons

--- ControlsTouch.gd
+++ ControlsTouch.gd
@@ -17,13 +17,33 @@ func _on_touch_screen_button_d_pressed():
 	Input.action_press("Right")
 
 
-func _on_touch_screen_button_f_pressed():
-	Input.action_press("Fire")
-
-
 func _on_touch_screen_button_r_pressed():
 	Input.action_press("Reload")
 
 
 func _on_touch_screen_button_e_pressed():
 	Input.action_press("Menu")
+
+
+func _on_touch_screen_button_r_released() -> void:
+	Input.action_release("Reload")
+
+
+func _on_touch_screen_button_w_released() -> void:
+	Input.action_release("Forward")
+
+
+func _on_touch_screen_button_a_released() -> void:
+	Input.action_release("Left")
+
+
+func _on_touch_screen_button_s_released() -> void:
+	Input.action_release("Back")
+
+
+func _on_touch_screen_button_d_released() -> void:
+	Input.action_release("Right")
+
+
+func _on_touch_screen_button_e_released() -> void:
+	Input.action_release("Menu")
